eb06e5224d93bbf64cff4e2d032d9672d6b15687
Vpox7 <148094617+WizzerX@users.noreply.github.com>
2025-04-10 14:28:45 +0530

    recover

---
+++ Include_Files/main.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+#include "../Header/Student.h"
+
+
+using namespace std;
+
+void main() {
+	vector<Data>student;
+
+	Data hen;
+	hen.AddData();
+	student.push_back(hen);
+	hen.display();
+
+
+	
+
+
+
+}
--- src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-//#include "../../../../../../../../../WebDeve/c++/student_management_System/student_management_System/Student.h"
-
-using namespace std;
-void main() {
-	cout << "hello" << endl;
-	
-
-}
