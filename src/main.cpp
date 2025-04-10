71128d60b0aa15f0dcc74a0813507c9e59da6e54
Vpox7 <148094617+WizzerX@users.noreply.github.com>
2025-04-10 11:54:39 +0530

    commit

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+//#include "../../../../../../../../../WebDeve/c++/student_management_System/student_management_System/Student.h"
+
+using namespace std;
+void main() {
+	cout << "hello" << endl;
+	
+
+}
